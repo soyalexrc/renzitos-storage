710873dea975ce155e77cfcd769c2cc07d6b34ba
alexrodriguez <alexcarvajal2404@gmail.com>
2024-12-02 10:47:50 -0500

    :sparkles: native folders

---
+++ android/app/src/main/java/com/alexrc/renzitosstorage/MainActivity.java
@@ -0,0 +1,5 @@
+package com.alexrc.renzitosstorage;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.alexrc.renzitosstorage',
   appName: 'renzitos-storage',
-  webDir: 'www'
+  webDir: 'www',
 };
 
 export default config;
